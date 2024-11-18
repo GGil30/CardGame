74155b0a10faa097593bbf6ee9bd3b6c8cb4c0dc
GGil30 <gabriel.gil@menloschool.org>
2024-11-18 09:38:25 -0800

    Finished the Card class.

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
+// Gabriel Gil, 11/18/24
 public class Card {
+    // Instance variables
+    private char rank;
+    private String suit;
+    private int value;
+
+    // Constructor
+    public Card(char rank, String suit, int value) {
+        this.rank = rank;
+        this.suit = suit;
+        this.value = value;
+    }
+
+    // Getters and Setters
+    public char getRank() {
+        return rank;
+    }
+
+    public void setRank(char rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String toString()
+    {
+        return this.rank + " of " + this.suit;
+    }
 }
