a9b450310f524095276b795461d7fd3afada7d45
GGil30 <gabriel.gil@menloschool.org>
2025-02-11 11:56:00 -0800

    Worked on drawing the background and printing the instructions for the beginning screen.

--- src/GameView.java
+++ src/GameView.java
@@ -3,13 +3,27 @@ import javax.swing.*;
 
 public class GameView extends JFrame {
     // Instance Variables
-    public static final int WINDOW_WIDTH = 1000;
+    public static final int WINDOW_WIDTH = 1425;
     public static final int WINDOW_HEIGHT = 1000;
+    private Image background;
+    private Image heart;
+    private Image spade;
+    private Image diamond;
+    private Image club;
+    private Image circle;
+
     Game game;
 
     // constructor
     public GameView(Game game){
         this.game = game;
+        this.background = new ImageIcon("Resources/background.jpg").getImage();
+        this.heart = new ImageIcon("Resources/heart.png").getImage();
+        this.spade = new ImageIcon("Resources/spade.png").getImage();
+        this.diamond = new ImageIcon("Resources/diamond.png").getImage();
+        this.club = new ImageIcon("Resources/club.png").getImage();
+        this.circle = new ImageIcon("Resources/circle.png").getImage();
+
 
 
         // Setup the window.
@@ -21,6 +35,32 @@ public class GameView extends JFrame {
 
     public void paint(Graphics g){
         g.drawRect(0,0, WINDOW_WIDTH, WINDOW_HEIGHT);
+        paintBackground(g);
+        paintInstructions(g);
+
+    }
+
+    public void paintBackground(Graphics g){
+        g.drawImage(background, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, this);
+        g.setColor(Color.black);
+        for(int i = 0; i < 4; i++){
+            if(i<2){
+                g.drawRect(25 + i*125, 60, 100, 140);
+            }
+            g.drawRect(WINDOW_WIDTH - 125 - i*125, 60, 100, 140);
+        }
+        g.drawImage(heart, 925, 60, 100, 140, this);
+        g.drawImage(club, 1050, 60, 100, 140, this);
+        g.drawImage(diamond, 1175, 60, 100, 140, this);
+        g.drawImage(spade, 1300, 60, 100, 140, this);
+    }
+
+    public void paintInstructions(Graphics g){
+        g.setColor(Color.white);
+        g.drawString("Welcome to Solitaire! If you aren't familiar with the game, the goal is to put all the cards in order by suit in the final columns. In this version, you will be shown the board before ", 100, 300);
+        g.drawString("each move with the choice of getting the next wild card or playing a move. The game will end automatically once you have won, but if you find yourself without any moves, then you've ", 100, 320);
+        String s = "unfortunately lost and will need to restart the game yourself. Good luck, " + game.player.getName() + "!";
+        g.drawString(s, 100, 340);
 
     }
 }
